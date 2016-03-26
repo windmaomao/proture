7e13a3bf8ce3a6a00d26d57a27a63b19be8210e2
Fang Jin <windmaomao@gmail.com>
2016-03-26 15:33:23 -0400

    assemble fields straight from model

--- mobile/admin/app.js
+++ mobile/admin/app.js
@@ -13,24 +13,28 @@ angular
         var nga = NgAdminConfigurationProvider;
         var opt = NgAdminOptions;
         var entities = {};
+        var models = {};
 
         // assemble nga fields based on field array
-        var assembleFields = function(p, fields) {
+        var assembleFields = function(fields) {
             var f = [];
             _.each(fields, function(field) {
                 if (!(field === Object(field))) {
-                    f.push(p.field(field));
+                    f.push(nga.field(field));
                 } else {
                     switch (field.type) {
+                        case 'string':
+                            f.push(nga.field(field.field));
+                            break;
                         case 'boolean':
-                            f.push(p.field(field.field, field.type));
+                            f.push(nga.field(field.field, field.type));
                             break;
                         case 'reference':
                             f.push(
-                                p.field(field.field, field.type)
+                                nga.field(field.field, field.type)
                                     .label('Company')
                                     .targetEntity(entities['company'])
-                                    .targetField(p.field('name'))
+                                    .targetField(nga.field('name'))
                             );
                             break;
                         default:
@@ -46,10 +50,68 @@ angular
             return [p.field(fields[0]).label(fields[0]).pinned(true)];
         };
 
+        // convert model to nga fields
+        var fieldsFromModel = function(model, fields) {
+            var updated = {};
+
+            _.each(fields, function(field) {
+                var updatedField = {};
+                var modelField = {};
+                if (!(field === Object(field))) {
+                    updatedField.field = field;
+                    if (!(model[field] === Object(model[field]))) {
+                        modelField = { type: model[field] };
+                    } else {
+                        modelField = model[field];
+                    }
+                    updatedField = _.defaults(updatedField, modelField);
+                } else {
+                    updatedField = field;
+                }
+                updated[updatedField.field] = updatedField;
+            });
+
+            return updated;
+        };
+
+        // assemble entity fields from models,
+        // givien entity name and fields
+        var entityModelFields = function(entity, fields) {
+            var nlist = [];
+            _.each(fields, function(field) {
+                var updated = {};
+                if (!(field === Object(field))) {
+                    updated = models[entity][field];
+                } else {
+                    updated = _.defaults(
+                        field,
+                        models[entity][field.field]
+                    );
+                }
+                nlist.push(updated);
+            });
+            return nlist;
+        };
+
+        // Assemble NGA models straight from Model
+        var ngaFieldsFromModel = function(model, fields) {
+            return assembleFields(entityModelFields(model, fields));
+        };
+
         // create an admin application
         var admin = nga.application(opt.site).baseApiUrl(opt.url);
 
         _.each(opt.entities, function(op, key) {
+            // skip inactive entity
+            var disable = op.disable || false;
+            if (disable) {
+                return;
+            }
+
+            var entityName = op.entity || key;
+            // get model fields
+            models[entityName] = fieldsFromModel(op.model, op.fields);
+
             var id = op.id || 'id';
             var fields = op.fields;
             var listFields = op.list.fields || fields;
@@ -57,26 +119,26 @@ angular
             var creationFields = op.creation.fields || fields;
             var searchFields = op.search.fields || id;
 
-            var entity = nga.entity(key).identifier(nga.field(id));
+            var entity = nga.entity(entityName).identifier(nga.field(id));
 
             entity.listView()
-                .fields(assembleFields(nga, listFields))
+                .fields(ngaFieldsFromModel(entityName, listFields))
                 .listActions(['show', 'edit'])
                 .filters(assembleSearchFields(nga, searchFields))
             ;
             entity.creationView()
-                .fields(assembleFields(nga, creationFields))
+                .fields(ngaFieldsFromModel(entityName, creationFields))
             ;
             entity.editionView()
-                .fields(assembleFields(nga, creationFields))
+                .fields(ngaFieldsFromModel(entityName, creationFields))
                 .title('Edit')
             ;
             entity.showView()
-                .fields(assembleFields(nga, showFields))
+                .fields(ngaFieldsFromModel(entityName, showFields))
                 .title('Detail')
             ;
 
-            entities[key] = entity;
+            entities[entityName] = entity;
             admin.addEntity(entity);
         });
 
--- mobile/admin/config.js
+++ mobile/admin/config.js
@@ -14,6 +14,7 @@ angular
         url: '/v1/',
         entities: {
             company: {
+                disable: true,
                 id: '_id',
                 fields: [
                     '_id', 'name', 'alias', 'slogan', 'active',
@@ -35,9 +36,19 @@ angular
                     fields: [
                         'name'
                     ]
-                }
+                },
+                model: {
+                    _id: { type: 'id' },
+                    name: { type: 'string', required: true },
+                    alias: { type: 'string', required: true },
+                    slogan: 'string',
+                    active: 'boolean',
+                    createdAt: 'date',
+                    updatedAt: 'date'
+                },
             },
             project: {
+                disable: true,
                 id: '_id',
                 fields: [
                     '_id', 'companyId', 'name', 'alias', 'active',
@@ -71,8 +82,94 @@ angular
                     fields: [
                         'name'
                     ]
+                },
+                model: {
+                    _id: { type: 'id' },
+                    companyId: { type: 'id', ref: 'company'},
+                    name: { type: 'string', required: true },
+                    alias: { type: 'string', required: true },
+                    description: 'string',
+                    active: 'boolean',
+                    createdAt: 'date',
+                    updatedAt: 'date'
                 }
             },
+            company: {
+                entity: 'company',
+                model: {
+                    _id: { type: 'id' },
+                    name: { type: 'string', required: true },
+                    alias: { type: 'string', required: true },
+                    slogan: 'string',
+                    active: 'boolean',
+                    createdAt: 'date',
+                    updatedAt: 'date'
+                },
+                id: '_id',
+                fields: [
+                    '_id', 'name', 'alias', 'slogan', 'active',
+                    'createdAt', 'updatedAt'
+                ],
+                list: {
+                    fields: [
+                        'name', 'slogan', 'alias', 'active',
+                    ]
+                },
+                creation: {
+                    fields: [
+                        'name', 'alias', 'slogan', 'active',
+                    ]
+                },
+                edition: {},
+                show: {},
+                search: {
+                    fields: [
+                        'name'
+                    ]
+                },
+            },
+            // project: {
+            //     entity: 'project',
+            //     model: {
+            //         _id: { type: 'id' },
+            //         companyId: { type: 'id', ref: 'company'},
+            //         name: { type: 'string', required: true },
+            //         alias: { type: 'string', required: true },
+            //         description: 'string',
+            //         active: 'boolean',
+            //         createdAt: 'date',
+            //         updatedAt: 'date'
+            //     },
+            //     id: '_id',
+            //     fields: [
+            //         '_id',
+            //         {
+            //             field: 'companyId',
+            //             type: 'reference',
+            //             targetEntity: 'company',
+            //             targetField: 'name'
+            //         },
+            //         'name', 'alias', 'active',
+            //         'createdAt', 'updatedAt'
+            //     ],
+            //     list: {
+            //         fields: [
+            //             'companyId', 'name', 'alias', 'active'
+            //         ]
+            //     },
+            //     creation: {
+            //         fields: [
+            //             'companyId', 'name', 'alias', 'active',
+            //         ]
+            //     },
+            //     edition: {},
+            //     show: {},
+            //     search: {
+            //         fields: [
+            //             'name'
+            //         ]
+            //     },
+            // },
         },
     })
 ;
